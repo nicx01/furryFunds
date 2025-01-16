259a15c79d04e144e170709e0d85c5138ade4e81
Huguinii <Vespertino@C09PC07>
2025-01-16 17:22:43 +0100

    rebase

--- app/src/main/java/com/nodejes/furryfunds/VistaRetos.java
+++ app/src/main/java/com/nodejes/furryfunds/VistaRetos.java
@@ -11,17 +11,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class VistaRetos extends AppCompatActivity{
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            EdgeToEdge.enable(this);
-            setContentView(R.layout.vistaretos_vista);
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.vistaRetos), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.vistaretos_vista);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.vistaRetos), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        }
+    }
+
+    public void CrearReto(View v) {
+        Intent intent = new Intent(this, VistaRetoIndividual.class);
+        startActivity(intent);
+    }
+
     public void InicioViewRetos(View v) {
         Intent intent = new Intent(this, VistaInicio.class);
         startActivity(intent);
